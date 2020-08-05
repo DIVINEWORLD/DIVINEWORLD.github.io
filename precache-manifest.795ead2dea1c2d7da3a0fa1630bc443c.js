7452ce7d62064ab590aaa92c38e9b99a9dfb416c
DominikHor <dhorstkamp@aol.com>
2020-08-05 15:45:20 +0200

    Updates

---
+++ precache-manifest.795ead2dea1c2d7da3a0fa1630bc443c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801c89fdb15c943f7101db4288933627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc953cb0d0ac2508ddcf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3e77cd6543685dc216f",
+    "url": "/static/js/2.0d6067fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d6067fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc953cb0d0ac2508ddcf",
+    "url": "/static/js/main.8982daf3.chunk.js"
+  },
+  {
+    "revision": "61f29d79965a49d91442",
+    "url": "/static/js/runtime-main.2aed2e31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
